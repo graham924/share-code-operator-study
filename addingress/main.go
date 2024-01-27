f1b677c32dbe206b52ae5bad89740bbf52549814
graham924 <1369760658@qq.com>
2024-01-27 22:25:33 +0800

    add comment

--- addingress/main.go
+++ addingress/main.go
@@ -3,28 +3,46 @@ package main
 import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"log"
 	"share-code-operator-study/addingress/pkg"
 )
 
 func main() {
+	// 创建一个 集群客户端配置
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
-		panic(err.Error())
+		inClusterConfig, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalln("can't get config")
+		}
+		config = inClusterConfig
 	}
+
+	// 创建一个 clientset 客户端，用于创建 informerFactory
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// 创建一个 informerFactory
 	factory := informers.NewSharedInformerFactory(clientset, 0)
+	// 使用 informerFactory 创建Services资源的 informer对象
 	serviceInformer := factory.Core().V1().Services()
+	// 使用 informerFactory 创建Ingresses资源的 informer对象
 	ingressInformer := factory.Networking().V1().Ingresses()
+
+	// 创建一个自定义控制器
 	controller := pkg.NewController(clientset, serviceInformer, ingressInformer)
 
+	// 创建 停止channel信号
 	stopCh := make(chan struct{})
+	// 启动 informerFactory，会启动已经创建的 serviceInformer、ingressInformer
 	factory.Start(stopCh)
+	// 等待 所有informer 从 etcd 实现全量同步
 	factory.WaitForCacheSync(stopCh)
 
+	// 启动自定义控制器
 	controller.Run(stopCh)
 }
--- addingress/pkg/controller.go
+++ addingress/pkg/controller.go
@@ -20,11 +20,15 @@ import (
 )
 
 const (
-	workNum  = 5
-	annoKey  = "ingress/http:true"
+	// worker 数量
+	workNum = 5
+	// service 指定 ingress 的 annotation key
+	annoKey = "ingress/http"
+	// 调谐失败的最大重试次数
 	maxRetry = 10
 )
 
+// 自定义控制器
 type controller struct {
 	client        kubernetes.Interface
 	serviceLister listercorev1.ServiceLister
@@ -32,7 +36,9 @@ type controller struct {
 	queue         workqueue.RateLimitingInterface
 }
 
+// NewController 创建一个自定义控制器
 func NewController(clientset *kubernetes.Clientset, serviceInformer informercorev1.ServiceInformer, ingressInformer informernetv1.IngressInformer) *controller {
+	// 控制器中，包含一个clientset、service和ingress的缓存监听器、一个workqueue
 	c := controller{
 		client:        clientset,
 		serviceLister: serviceInformer.Lister(),
@@ -40,36 +46,34 @@ func NewController(clientset *kubernetes.Clientset, serviceInformer informercore
 		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ingressManager"),
 	}
 
+	// 为 serviceInformer 添加 ResourceEventHandler
 	serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    c.addService,
+		// 添加service时触发
+		AddFunc: c.addService,
+		// 修改service时触发
 		UpdateFunc: c.updateService,
+		// 这里没有删除service的逻辑，因为我们会使用 OwnerReferences 将service+ingress关联起来。
+		// 因此删除service，会由kubernetes的ControllerManager中的特殊Controller，自动完成ingress的gc
 	})
 
+	// 为 ingressInformer 添加 ResourceEventHandler
 	ingressInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// 删除ingress时触发
 		DeleteFunc: c.deleteIngress,
 	})
 
 	return &c
 }
 
-func (c *controller) enqueue(obj interface{}) {
-	key, err := cache.MetaNamespaceKeyFunc(obj)
-	if err != nil {
-		runtime.HandleError(err)
-		return
-	}
-	c.queue.Add(key)
-}
-
-func (c *controller) dequeue(item interface{}) {
-	c.queue.Done(item)
-}
-
+// 添加service时触发
 func (c *controller) addService(obj interface{}) {
+	// 将 待添加service 的 key 加入 workqueue
 	c.enqueue(obj)
 }
 
+// 修改service时触发
 func (c *controller) updateService(oldObj interface{}, newObj interface{}) {
+	// 如果两个对象一致，就无需触发修改逻辑
 	if reflect.DeepEqual(oldObj, newObj) {
 		return
 	}
@@ -77,10 +81,7 @@ func (c *controller) updateService(oldObj interface{}, newObj interface{}) {
 	c.enqueue(newObj)
 }
 
-func (c *controller) deleteService(obj interface{}) {
-	c.enqueue(obj)
-}
-
+// 删除ingress时触发
 func (c *controller) deleteIngress(obj interface{}) {
 	ingress := obj.(*netv1.Ingress)
 	ownerReference := metav1.GetControllerOf(ingress)
@@ -90,45 +91,84 @@ func (c *controller) deleteIngress(obj interface{}) {
 	c.enqueue(obj)
 }
 
+// enqueue 将 待添加service 的 key 加入 workqueue
+func (c *controller) enqueue(obj interface{}) {
+	// 调用工具方法，获取 kubernetes资源对象的 key（默认是 ns/name，或 name）
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	// 获取失败，不加入队列，即本次事件不予处理
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	// 将 key 加入 workqueue
+	c.queue.Add(key)
+}
+
+// dequeue 将处理完成的 key 出队
+func (c *controller) dequeue(item interface{}) {
+	c.queue.Done(item)
+}
+
+// Run 启动controller
 func (c *controller) Run(stopCh chan struct{}) {
+	// 启动多个worker，同时对workqueue中的事件进行处理，效率提升5倍
 	for i := 0; i < workNum; i++ {
+		// 每个worker都是一个协程，使用同一个停止信号
 		go wait.Until(c.worker, time.Minute, stopCh)
 	}
+	// 启动完成后，Run函数就停止在这里，等待停止信号
 	<-stopCh
 }
 
+// worker方法
 func (c *controller) worker() {
+	// 死循环，worker处理完一个，再去处理下一个
 	for c.processNextItem() {
 
 	}
 }
 
+// processNextItem 处理下一个
 func (c *controller) processNextItem() bool {
+	// 从 workerqueue 取出一个key
 	item, shutdown := c.queue.Get()
+	// 如果已经收到停止信号了，则返回false，worker就会停止处理
 	if shutdown {
 		return false
 	}
+	// 处理完成后，将这个key出队
 	defer c.dequeue(item)
 
+	// 转成string类型的key
 	key := item.(string)
+
+	// 处理service逻辑的核心方法
 	err := c.syncService(key)
+	// 处理过程出错，进入错误统一处理逻辑
 	if err != nil {
 		c.handleError(key, err)
 	}
+	// 处理结束，返回true
 	return true
 }
 
+// handleError 错误统一处理逻辑
 func (c *controller) handleError(key string, err error) {
+	// 如果当前key的处理次数，还不到最大重试次数，则再次加入队列
 	if c.queue.NumRequeues(key) < maxRetry {
 		c.queue.AddRateLimited(key)
 		return
 	}
 
+	// 运行时统一处理错误
 	runtime.HandleError(err)
+	// 不再处理这个key
 	c.queue.Forget(key)
 }
 
+// syncService 处理service逻辑的核心方法
 func (c *controller) syncService(key string) error {
+	// 将 key 切割为 ns 和 name
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
 		return err
@@ -144,26 +184,24 @@ func (c *controller) syncService(key string) error {
 		return err
 	}
 
-	// 检查service的annotation，是否包含 key: "ingress/http:true"
+	// 检查service的annotation，是否包含 key: "ingress/http"
 	_, ok := service.Annotations[annoKey]
-	// 从indexer中，获取ingress
+	// 从indexer缓存中，获取ingress
 	ingress, err := c.ingressLister.Ingresses(namespace).Get(name)
 
-	// service有"ingress/http:true"，但是ingress不存在，需要创建ingress
 	if ok && errors.IsNotFound(err) {
-		return nil
-	}
-
-	if !ok && ingress != nil {
-		// ingress存在，但是service没有"ingress/http:true"，需要删除ingress
-		err := c.client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+		// ingress不存在，但是service有"ingress/http"，需要创建ingress
+		// 创建ingress
+		ig := c.createIngress(service)
+		// 调用controller中的client，完成ingress的创建
+		_, err := c.client.NetworkingV1().Ingresses(namespace).Create(context.TODO(), ig, metav1.CreateOptions{})
 		if err != nil {
 			return err
 		}
-	} else if ok && ingress == nil {
-		// ingress不存在，但是service存在，需要创建ingress
-		ig := c.createIngress(service)
-		_, err := c.client.NetworkingV1().Ingresses(namespace).Create(context.TODO(), ig, metav1.CreateOptions{})
+	} else if !ok && ingress != nil {
+		// ingress存在，但是service没有"ingress/http"，需要删除ingress
+		// 调用controller中的client，完成ingress的删除
+		err := c.client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
 		if err != nil {
 			return err
 		}
@@ -172,6 +210,7 @@ func (c *controller) syncService(key string) error {
 	return nil
 }
 
+// createIngress 创建ingress
 func (c *controller) createIngress(service *corev1.Service) *netv1.Ingress {
 	icn := "ingress"
 	pathType := netv1.PathTypePrefix
