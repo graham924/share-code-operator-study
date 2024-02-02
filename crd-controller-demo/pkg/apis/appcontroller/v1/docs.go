02903f55fec5dcae49656a3fd322254d4789d6ac
grahamzhu <zgy1369760658@163.com>
2024-02-03 00:07:00 +0800

    test

--- crd-controller-demo/pkg/apis/appcontroller/v1/docs.go
+++ crd-controller-demo/pkg/apis/appcontroller/v1/docs.go
@@ -1,2 +1,4 @@
+// +k8s:deepcopy-gen=package
 // +groupName=appcontroller.k8s.io
+
 package v1
--- crd-controller-demo/pkg/apis/appcontroller/v1/types.go
+++ crd-controller-demo/pkg/apis/appcontroller/v1/types.go
@@ -6,31 +6,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type DeploymentTemplate struct {
-	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
-	Spec              appsv1.DeploymentSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
-}
-
-type ServiceTemplate struct {
-	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
-	Spec              corev1.ServiceSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
-}
-
-// AppSpec defines the desired state of App
-type AppSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS -- desired state of cluster
-	DeploymentTemplate DeploymentTemplate `json:"deploymentTemplate,omitempty"`
-	ServiceTemplate    ServiceTemplate    `json:"serviceTemplate,omitempty"`
-}
-
-// AppStatus defines the observed state of App.
-// It should always be reconstructable from the state of the cluster and/or outside world.
-type AppStatus struct {
-	// INSERT ADDITIONAL STATUS FIELDS -- observed state of cluster
-	DeploymentStatus appsv1.DeploymentStatus `json:"deploymentStatus,omitempty"`
-	ServiceStatus    corev1.ServiceStatus    `json:"serviceStatus,omitempty"`
-}
-
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -41,6 +16,35 @@ type App struct {
 
 	Spec   AppSpec   `json:"spec,omitempty"`
 	Status AppStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AppSpec defines the desired state of App
+type AppSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS -- desired state of cluster
+	DeploymentTemplate DeploymentTemplate `json:"deploymentTemplate,omitempty"`
+	ServiceTemplate    ServiceTemplate    `json:"serviceTemplate,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AppStatus defines the observed state of App.
+// It should always be reconstructable from the state of the cluster and/or outside world.
+type AppStatus struct {
+	// INSERT ADDITIONAL STATUS FIELDS -- observed state of cluster
+	DeploymentStatus appsv1.DeploymentStatus `json:"deploymentStatus,omitempty"`
+	ServiceStatus    corev1.ServiceStatus    `json:"serviceStatus,omitempty"`
+}
+
+type DeploymentTemplate struct {
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec              appsv1.DeploymentSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+}
+
+type ServiceTemplate struct {
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec              corev1.ServiceSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
